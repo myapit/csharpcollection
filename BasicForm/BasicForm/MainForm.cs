a17643d8533e7466e92e671836a1aaae9ec58718
myapit <hafeez.cs@gmail.com>
2019-08-05 16:49:56 +0800

    add basic form with messageboxmanager

---
+++ BasicForm/BasicForm/MainForm.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: domokun
+ * Date: 2019-06-25
+ * Time: 10:57 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BasicForm
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Owh, hi, am i running on Arch Linux ? if yes, i'm very happy");
+		}
+		
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("This is a message...", "Test", MessageBoxButtons.OKCancel);
+		}
+	}
+	
+	
+
+
+
+}
---
+++ BasicForm/BasicForm/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: domokun
+ * Date: 2019-06-25
+ * Time: 10:57 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace BasicForm
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			/* Begin MessageBoxManager Setting */
+			MessageBoxManager.OK = "Alright";
+            MessageBoxManager.Cancel = "Noway";
+           	MessageBoxManager.Register();
+			/* End MessageBoxManager Setting */
+			Application.Run(new MainForm());
+			MessageBoxManager.Unregister();
+			
+		}
+		
+	}
+}
