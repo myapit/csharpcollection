d63f88006a44c97f11b73c7b40215c3e87667cd9
myapit <hafeez.cs@gmail.com>
2019-08-23 17:10:11 +0800

    datepicker

--- BasicForm/BasicForm/MainForm.Designer.cs
+++ BasicForm/BasicForm/MainForm.Designer.cs
@@ -40,6 +40,7 @@ namespace BasicForm
 		{
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -62,17 +63,26 @@ namespace BasicForm
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.Button2Click);
 			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePicker1.Location = new System.Drawing.Point(323, 68);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(101, 20);
+			this.dateTimePicker1.TabIndex = 2;
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(436, 261);
+			this.ClientSize = new System.Drawing.Size(707, 283);
+			this.Controls.Add(this.dateTimePicker1);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "BasicForm";
 			this.ResumeLayout(false);
-
 		}
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
 	}
 }
