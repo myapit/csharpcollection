24d1b1f33a119e27dcd74482b74eaf6d0f323461
myapit <hafeez.cs@gmail.com>
2019-08-26 16:15:24 +0800

    Add HASH and user password login

--- BasicForm/BasicForm/MainForm.Designer.cs
+++ BasicForm/BasicForm/MainForm.Designer.cs
@@ -41,6 +41,12 @@ namespace BasicForm
 			this.button1 = new System.Windows.Forms.Button();
 			this.button2 = new System.Windows.Forms.Button();
 			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.button3 = new System.Windows.Forms.Button();
+			this.txtUser = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPass = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCheck = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -71,18 +77,82 @@ namespace BasicForm
 			this.dateTimePicker1.Size = new System.Drawing.Size(101, 20);
 			this.dateTimePicker1.TabIndex = 2;
 			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(539, 83);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(103, 36);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Read Txt File";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// txtUser
+			// 
+			this.txtUser.Location = new System.Drawing.Point(503, 162);
+			this.txtUser.Name = "txtUser";
+			this.txtUser.Size = new System.Drawing.Size(100, 20);
+			this.txtUser.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(425, 162);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 23);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "user";
+			// 
+			// txtPass
+			// 
+			this.txtPass.Location = new System.Drawing.Point(503, 189);
+			this.txtPass.Name = "txtPass";
+			this.txtPass.PasswordChar = '#';
+			this.txtPass.Size = new System.Drawing.Size(100, 20);
+			this.txtPass.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(425, 185);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 23);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "pass";
+			// 
+			// btnCheck
+			// 
+			this.btnCheck.Location = new System.Drawing.Point(571, 226);
+			this.btnCheck.Name = "btnCheck";
+			this.btnCheck.Size = new System.Drawing.Size(75, 23);
+			this.btnCheck.TabIndex = 8;
+			this.btnCheck.Text = "check";
+			this.btnCheck.UseVisualStyleBackColor = true;
+			this.btnCheck.Click += new System.EventHandler(this.BtnCheckClick);
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(707, 283);
+			this.ClientSize = new System.Drawing.Size(845, 321);
+			this.Controls.Add(this.btnCheck);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPass);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtUser);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.dateTimePicker1);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "BasicForm";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button btnCheck;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtPass;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtUser;
+		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.DateTimePicker dateTimePicker1;
 	}
 }
--- BasicForm/BasicForm/MainForm.cs
+++ BasicForm/BasicForm/MainForm.cs
@@ -10,6 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;  
 
 namespace BasicForm
 {
@@ -39,10 +42,71 @@ namespace BasicForm
 		{
 			MessageBox.Show("This is a message...", "Test", MessageBoxButtons.OKCancel);
 		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			//static readonly 
+				string textFile = @"auth.db";
+			
+			if (!File.Exists(textFile))
+			{
+				string createText = "admin" + Environment.NewLine +"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+				File.WriteAllText(textFile, createText);
+								
+			}else{
+				string[] lines = File.ReadAllLines(textFile);
+				foreach (string line in lines)  
+				System.Diagnostics.Debug.WriteLine(line);
+			}
+			/*
+			 * using(StreamWriter writetext = new StreamWriter("write.txt"))
+			{
+			writetext.WriteLine("writing in text file");
+			}
+			Reading a text file:
+			
+			using(StreamReader readtext = new StreamReader("readme.txt"))
+			{
+			string readMeText = readtext.ReadLine();
+			}
+			*/
+		}//end btn
+
+ 		static string ComputeSha256Hash(string rawData)  
+        {  
+              byte[] bytes = Encoding.UTF8.GetBytes(rawData);
+		        SHA256Managed hashstring = new SHA256Managed();
+		        byte[] hash = hashstring.ComputeHash(bytes);
+		        string hashString = string.Empty;
+		        foreach (byte x in hash)
+		        {
+		            hashString += String.Format("{0:x2}", x);
+		        }
+		        return hashString;
+ 		}
+
+
+		
+		void BtnCheckClick(object sender, EventArgs e)
+		{
+			//static readonly 
+				string textFile = @"auth.db";
+
+				string[] lines = File.ReadAllLines(textFile);
+				//foreach (string line in lines)  
+				System.Diagnostics.Debug.WriteLine(ComputeSha256Hash(txtPass.Text));
+				System.Diagnostics.Debug.WriteLine(lines[1]);
+				
+				if ( txtUser.Text == lines[0] && ComputeSha256Hash(txtPass.Text) == lines[1])
+				{
+					MessageBox.Show("correct");
+				}else{
+					MessageBox.Show(" in Correct");
+				}
+				if (Control.IsKeyLocked(Keys.CapsLock)) {  
+				    MessageBox.Show("The Caps Lock key is ON.");  
+				} 
+		}
 	}
 	
-	
-
-
-
 }
