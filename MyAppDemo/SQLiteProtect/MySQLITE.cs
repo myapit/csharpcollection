50c9305603cf99d026f919552c482279eb8b98dc
myapit <hafeez.cs@gmail.com>
2020-07-24 11:53:17 +0800

    in progress searching sqlite db protection method

--- MyAppDemo/SQLiteProtect/MySQLITE.cs
+++ MyAppDemo/SQLiteProtect/MySQLITE.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
 using System.Data.SQLite;
-using System.Data.Common;
+using System.IO;
+using System.Windows.Forms;
 
 namespace SQLiteProtect
 {
@@ -22,10 +14,10 @@ namespace SQLiteProtect
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog myfile  = new OpenFileDialog();
+            OpenFileDialog myfile = new OpenFileDialog();
             myfile.Filter = "SQLite Database |*.db; *.db3";
 
-            if ( myfile.ShowDialog() == DialogResult.OK)
+            if (myfile.ShowDialog() == DialogResult.OK)
             {
                 string mypath = null;
                 Path.GetExtension(myfile.FileName);
@@ -38,8 +30,25 @@ namespace SQLiteProtect
         {
             SQLiteConnection myconn = new SQLiteConnection("Data Source=" + textBox1.Text + ";Version=3");
             myconn.Open();
-           /// sqlite password tak support dah laaaa
 
+            //myconn.ChangePassword();
+            /// sqlite password tak support dah laaaa
+
+            //    var connectionString = new SqliteConnectionStringBuilder("Data Source=" + textBox1.Text + ";Version=3")
+            //    {
+            //        Mode = SqliteOpenMode.ReadWriteCreate,
+            //        Password = password
+            //    }.ToString();
+            //    //To change the password you would do the following:
+
+            //var command = connection.CreateCommand();
+            //    command.CommandText = "SELECT quote($newPassword);";
+            //    command.Parameters.AddWithValue("$newPassword", newPassword);
+            //    var quotedNewPassword = (string)command.ExecuteScalar();
+
+            //    command.CommandText = "PRAGMA rekey = " + quotedNewPassword;
+            //    command.Parameters.Clear();
+            //    command.ExecuteNonQuery();
         }
     }
 }
